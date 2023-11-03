10c7e2f6d572d6233439f629e33cff522048a42f
hechsb <hichem2001.sboui@gmail.com>
2023-11-03 10:38:45 +0100

    dd

--- src/components/AddService/AddService.jsx
+++ src/components/AddService/AddService.jsx
@@ -1,50 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./AddService.scss";
 
 const AddService = () => {
+  const [inputs, setInputs] = useState({});
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+    // console.log(inputs)
+  }
+  useEffect(() => {
+    // This code will run whenever the 'inputs' state is updated.
+    console.log(inputs);
+  }, [inputs]);
   return (
     <div className="add">
       <div className="container">
-        <h1>Add New Gig</h1>
+        <h1>Add New Service</h1>
         <div className="sections">
           <div className="info">
             <label htmlFor="">Title</label>
             <input
               type="text"
-              placeholder="e.g. I will do something I'm really good at"
+              placeholder="give a title of your job"
+              name="title"
+              onChange={handleChange}
             />
             <label htmlFor="">Category</label>
-            <select name="cats" id="cats">
-              <option value="design">Design</option>
-              <option value="web">Web Development</option>
-              <option value="animation">Animation</option>
-              <option value="music">Music</option>
-            </select>
-            <label htmlFor="">Cover Image</label>
-            <input type="file" />
+            <select name="cats" id="cats" onChange={handleChange}>
+             <option value="design">Design</option>
+             <option value="web">Web Development</option>
+             <option value="animation">Animation</option>
+             <option value="music">Music</option>
+             </select>
             <label htmlFor="">Upload Images</label>
             <input type="file" multiple />
             <label htmlFor="">Description</label>
-            <textarea name="" id="" placeholder="Brief descriptions to introduce your service to customers" cols="0" rows="16"></textarea>
-            <button>Create</button>
+            <textarea name="description" onChange={handleChange} id="" placeholder="Brief descriptions to introduce your service" cols="0" rows="16"></textarea>
           </div>
           <div className="details">
-            <label htmlFor="">Service Title</label>
-            <input type="text" placeholder="e.g. One-page web design" />
-            <label htmlFor="">Short Description</label>
-            <textarea name="" id="" placeholder="Short description of your service" cols="30" rows="10"></textarea>
+
             <label htmlFor="">Delivery Time (e.g. 3 days)</label>
-            <input type="number" />
-            <label htmlFor="">Revision Number</label>
-            <input type="number" />
+            <input type="number" name="deliveryTime" onChange={handleChange} />
             <label htmlFor="">Add Features</label>
-            <input type="text" placeholder="e.g. page design" />
-            <input type="text" placeholder="e.g. file uploading" />
-            <input type="text" placeholder="e.g. setting up a domain" />
-            <input type="text" placeholder="e.g. hosting" />
+            <input type="text" name="feature1" onChange={handleChange} placeholder="e.g. page design" />
+            <input type="text" name="feauture2" onChange={handleChange} placeholder="e.g. file uploading" />
             <label htmlFor="">Price</label>
-            <input type="number" />
+            <input type="number" name="price" onChange={handleChange}/>
+            <button>Create</button>
           </div>
+          
         </div>
       </div>
     </div>
