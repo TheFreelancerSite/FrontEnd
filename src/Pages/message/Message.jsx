d49904d1065a3ab0652a59292db17796d6997df9
hechsb <hichem2001.sboui@gmail.com>
2023-11-12 15:57:28 +0100

    the use now retreive the messages for a specific conversation

--- src/Pages/message/Message.jsx
+++ src/Pages/message/Message.jsx
@@ -2,15 +2,29 @@ import React from "react";
 import { Link , useParams  } from "react-router-dom";
 import "./Message.scss";
 import { useLocation } from "react-router-dom";
-
-const Message = (props) => {
+import { useEffect } from "react";
+import axios from "axios"
+import { useState } from "react";
+const Message = () => {
   // const location = useLocation();
   // const { conversationId, interactedWith } = location.state || {};*
   const { id, interactedWith } = useParams();
   const interactedWithObject = JSON.parse(decodeURIComponent(interactedWith));
-  console.log("this iss from the single messge " ,interactedWithObject)
+  const [conversation , setConversation]=useState([])
+  // console.log("this iss from the single messge " ,interactedWithObject , id)
+
+  useEffect(()=>{
+    axios.get(`http://localhost:3000/message/conversation/${id}`)
+    .then((response)=>{
+      setConversation(response.data.messages)
+      console.log("thissssss the conversation of a specific message",conversation)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  },[])
   return (
-    <div className="message">
+    <div className="message">   
       <div className="container">
         <span className="breadcrumbs">
           <Link to="/messages">Messages</Link> 
@@ -19,7 +33,7 @@ const Message = (props) => {
           <div className="item">
             <img
               src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
+              alt=""  
             />
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
