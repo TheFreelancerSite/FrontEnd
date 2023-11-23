748c424a52118b6ad8315cbeeb788ddb42875646
hechsb <hichem2001.sboui@gmail.com>
2023-11-23 18:15:47 +0100

    updating services and devides into 3 tables

--- src/Pages/MyServices/MyServices.jsx
+++ src/Pages/MyServices/MyServices.jsx
@@ -7,6 +7,9 @@ import { useSelector } from "react-redux";
 function MyServices() {
   const user = useSelector((state) => state.user.value);
   const [services, setServices] = useState([]);
+  const [pendingServices, setPendingServices] = useState([]);
+  const [activeServices, setActiveServices] = useState([]);
+  const [validatedServices, setValidatedServices] = useState([]);
   const currentUser = {
     id: 1,
     username: "Anna",
@@ -15,13 +18,40 @@ function MyServices() {
 
   useEffect(() => {
     axios.get(`http://localhost:3000/service/getServicesForSpecificUser/${user.userId}`)
-      .then((response) => {
+      .then(async (response) => {
         setServices(response.data);
+  
+        // Use map to return an array of promises
+        const servicePromises = response.data.map(async (service) => {
+          try {
+            const statusResponse = await axios.get(`http://localhost:3000/service/getServiceStatus/${service.id}`);
+            const { status, validated } = statusResponse.data;
+        
+            if (status === 'pending') {
+              setPendingServices((prevPendingServices) => [...prevPendingServices, service]);
+            } else if (status === 'accepted') {
+              
+        
+              if (validated) {
+                // Only add to validatedServices if both accepted and validated
+                setValidatedServices((prevValidatedServices) => [...prevValidatedServices, service]);
+              }
+              else{
+                setActiveServices((prevActiveServices) => [...prevActiveServices, service]);
+              }
+            }
+          } catch (error) {
+            console.log(error);
+          }
+        });
+  
+        // Use Promise.all to wait for all promises to resolve
+        await Promise.all(servicePromises);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, [user.userId]);
+  }, []);
 
   return (
     <div className="myGigs">
@@ -46,46 +76,69 @@ function MyServices() {
               </tr>
             </thead>
             <tbody>
-              {services.map((service) => (
+              {pendingServices.map((service) => (
                 <tr key={service.id}>
                   <td>{service.title}</td>
                   <td>{service.createdAt.slice(0, 10)}</td>
                   <td>{service.price}</td>
                   <td>
-                    <Link to={`/delete/${service.id}` } className="link">Delete</Link>|{" "}
+                    <Link to={`/delete/${service.id}`} className="link">Delete</Link>|{" "}
                     <Link to={`/applicant/${service.id}`} className="link">See applicants</Link>
-
                   </td>
                 </tr>
               ))}
             </tbody>
           </table>
         )}
-        <h1>My active Services</h1>
+        <h1>My Active Services</h1>
         <table>
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Created At</th>
-                <th>Price</th>
-                <th>Action</th>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Created At</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {activeServices.map((service) => (
+              <tr key={service.id}>
+                <td>{service.title}</td>
+                <td>{service.createdAt.slice(0, 10)}</td>
+                <td>{service.price}</td>
+                <td>
+                  <Link to={`/delete/${service.id}`} className="link">Delete</Link>|{" "}
+                  <Link to={`/applicant/${service.id}`} className="link">See applicants</Link>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {services.map((service) => (
-                <tr key={service.id}>
-                  <td>{service.title}</td>
-                  <td>{service.createdAt.slice(0, 10)}</td>
-                  <td>{service.price}</td>
-                  <td>
-                    <Link to={`/delete/${service.id}` } className="link">Delete</Link>|{" "}
-                    <Link to={`/applicant/${service.id}`} className="link">See applicants</Link>
+            ))}
+          </tbody>
+        </table>
 
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+        <h1>My Validated Services</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Created At</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {validatedServices.map((service) => (
+              <tr key={service.id}>
+                <td>{service.title}</td>
+                <td>{service.createdAt.slice(0, 10)}</td>
+                <td>{service.price}</td>
+                <td>
+                  <Link to={`/delete/${service.id}`} className="link">Delete</Link>|{" "}
+                  <Link to={`/applicant/${service.id}`} className="link">See applicants</Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
